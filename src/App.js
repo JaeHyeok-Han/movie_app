4545365ca4b127b50ca576e403cd7e2594b2c324
Han jaehyeok <hjh0633@google.com>
2020-10-08 12:26:43 +0900

    practice

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import React from 'react';
 
+function Food(props) {
+  return <div>
+      <h1>I love {props.fav}</h1>
+      <h3>descript: {props.des}</h3>
+    </div>
+}
+
+const myfood = [{name: "A", descript: "A is a"}, {name: "B", descript: "B is b"}, {name: "C", descript: "C is c"}];
+
 function App() {
   return (
-    <div className="App">
-      hello!
+    <div>
+      {myfood.map(function(current) {return <Food fav={current.name} des={current.descript}/>})}
     </div>
   );
 }
